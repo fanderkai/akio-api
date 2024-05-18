48ad2ee5407efb006bb78337846c546db20e0156
fathanahdz <dzsbnf21d@student.unhas.ac.id>
2024-05-18 08:04:25 +0800

    refactor index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose
   .then(() => {
     console.log("Connected to database!");
     app.listen(process.env.PORT || 3000, () => {
-      console.log("Server is running on port " + (process.env.PORT || 3000) + "!;
+      console.log("Server is running on port "+ (process.env.PORT || 3000));
     });
   })
   .catch(() => {
